ed471190c53db6c59a73213d3edd0589d896a318
Elijah Creed Fedele <physecfed@gmail.com>
2022-10-05 20:50:08 -0400

    Begun binding data model

---
+++ BloodPressure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPTracker
+{
+    public class BloodPressure
+    {
+        public DateTime MeasurementTime { get; set; }
+        public int SystolicPressure { get; set; }
+        public int DiastolicPressure { get; set; }
+        public int HeartRate { get; set; }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Data;
+
+namespace BPTracker
+{
+    public class MainViewModel : BindingBase
+    {
+        public List<BloodPressure> BloodPressureData;
+        public decimal SystolicMean, SystolicVariance;
+        public decimal DiastolicMean, DiastolicVariance;
+        public decimal HeartRateMean, HeartRateVariance;
+        public string SystolicString, DiastolicString, HeartRateString;
+
+        public MainViewModel()
+        {
+            BloodPressureData = new List<BloodPressure>();
+        }
+
+        public void UpdateStatistics()
+        {
+            int systolicSum = 0, diastolicSum = 0, heartRateSum = 0;
+            foreach (var bloodPressureElement in BloodPressureData) {
+                systolicSum += bloodPressureElement.SystolicPressure;
+                diastolicSum += bloodPressureElement.DiastolicPressure;
+                heartRateSum += bloodPressureElement.HeartRate;
+            }
+            SystolicMean = ((decimal) systolicSum) / ((decimal) BloodPressureData.Count);
+            DiastolicMean = ((decimal) diastolicSum) / ((decimal) BloodPressureData.Count);
+            HeartRateMean = ((decimal) heartRateSum) / ((decimal) BloodPressureData.Count);
+        }
+    }
+}
